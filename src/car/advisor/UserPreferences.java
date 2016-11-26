f2bf6a8ef46b918b05f659041c71c405496dedc6
januszekm3 <jmirosla@student.agh.edu.pl>
2016-11-26 11:15:54 +0100

    GUI - new panels

---
+++ src/car/advisor/UserPreferences.java
@@ -0,0 +1,140 @@
+package car.advisor;
+
+/**
+ * Created by Janusz on 26-11-2016.
+ */
+public class UserPreferences {
+    private String cena;
+    private String pojemnoscSilnika;
+    private String bagaznik;
+    private String iloscMiejsc;
+    private String wymiary;
+    private boolean przeznaczenieMiasto;
+    private boolean przeznaczenieTeren;
+    private boolean przeznaczenieTrasa;
+    private boolean przeznaczenieZazdroscSasiadow;
+    private boolean bogateWyposazenie;
+    private boolean komfortJazdy;
+    private boolean niskieKosztyEksploatacji;
+    private boolean wysokieOsiagi;
+
+    public String getCena() {
+        return cena;
+    }
+
+    public void setCena(String cena) {
+        this.cena = cena;
+    }
+
+    public String getPojemnoscSilnika() {
+        return pojemnoscSilnika;
+    }
+
+    public void setPojemnoscSilnika(String pojemnoscSilnika) {
+        this.pojemnoscSilnika = pojemnoscSilnika;
+    }
+
+    public String getBagaznik() {
+        return bagaznik;
+    }
+
+    public void setBagaznik(String bagaznik) {
+        this.bagaznik = bagaznik;
+    }
+
+    public String getIloscMiejsc() {
+        return iloscMiejsc;
+    }
+
+    public void setIloscMiejsc(String iloscMiejsc) {
+        this.iloscMiejsc = iloscMiejsc;
+    }
+
+    public String getWymiary() {
+        return wymiary;
+    }
+
+    public void setWymiary(String wymiary) {
+        this.wymiary = wymiary;
+    }
+
+    public boolean isPrzeznaczenieMiasto() {
+        return przeznaczenieMiasto;
+    }
+
+    public void setPrzeznaczenieMiasto(boolean przeznaczenieMiasto) {
+        this.przeznaczenieMiasto = przeznaczenieMiasto;
+    }
+
+    public boolean isPrzeznaczenieTeren() {
+        return przeznaczenieTeren;
+    }
+
+    public void setPrzeznaczenieTeren(boolean przeznaczenieTeren) {
+        this.przeznaczenieTeren = przeznaczenieTeren;
+    }
+
+    public boolean isPrzeznaczenieTrasa() {
+        return przeznaczenieTrasa;
+    }
+
+    public void setPrzeznaczenieTrasa(boolean przeznaczenieTrasa) {
+        this.przeznaczenieTrasa = przeznaczenieTrasa;
+    }
+
+    public boolean isPrzeznaczenieZazdroscSasiadow() {
+        return przeznaczenieZazdroscSasiadow;
+    }
+
+    public void setPrzeznaczenieZazdroscSasiadow(boolean przeznaczenieZazdroscSasiadow) {
+        this.przeznaczenieZazdroscSasiadow = przeznaczenieZazdroscSasiadow;
+    }
+
+    public boolean isBogateWyposazenie() {
+        return bogateWyposazenie;
+    }
+
+    public void setBogateWyposazenie(boolean bogateWyposazenie) {
+        this.bogateWyposazenie = bogateWyposazenie;
+    }
+
+    public boolean isKomfortJazdy() {
+        return komfortJazdy;
+    }
+
+    public void setKomfortJazdy(boolean komfortJazdy) {
+        this.komfortJazdy = komfortJazdy;
+    }
+
+    public boolean isNiskieKosztyEksploatacji() {
+        return niskieKosztyEksploatacji;
+    }
+
+    public void setNiskieKosztyEksploatacji(boolean niskieKosztyEksploatacji) {
+        this.niskieKosztyEksploatacji = niskieKosztyEksploatacji;
+    }
+
+    public boolean isWysokieOsiagi() {
+        return wysokieOsiagi;
+    }
+
+    public void setWysokieOsiagi(boolean wysokieOsiagi) {
+        this.wysokieOsiagi = wysokieOsiagi;
+    }
+
+    public String toString() {
+        return  "Cena: " + this.cena
+                + "\nPojemnosc silnika: " + this.pojemnoscSilnika
+                + "\nBagaznik: " + this.bagaznik
+                + "\nIlosc miejsc: " + this.iloscMiejsc
+                + "\nWymiary: " + this.wymiary
+                + "\nPrzeznaczenie miasto: " + String.valueOf(this.przeznaczenieMiasto)
+                + "\nPrzeznaczenie teren: " + String.valueOf(this.przeznaczenieTeren)
+                + "\nPrzeznaczenie trasa: " + String.valueOf(this.przeznaczenieTrasa)
+                + "\nPrzeznaczenie zazdrosc sasiadow: " + String.valueOf(this.przeznaczenieZazdroscSasiadow)
+                + "\nBogate wyposazenie: " + String.valueOf(this.bogateWyposazenie)
+                + "\nKomfort jazdy: " + String.valueOf(this.komfortJazdy)
+                + "\nNiskie koszty eksploatacji: " + String.valueOf(this.niskieKosztyEksploatacji)
+                + "\nWysokie osiagi: "  + String.valueOf(this.wysokieOsiagi);
+    }
+}
--- src/car/advisor/gui/Gui.java
+++ src/car/advisor/gui/Gui.java
@@ -1,5 +1,7 @@
 package car.advisor.gui;
 
+import car.advisor.UserPreferences;
+
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.ButtonGroup;
@@ -13,11 +15,14 @@ public class Gui {
         Gui.run();
     }
 
-    protected static void run () {
-        String ceny[] = { "bardzo_wysoka", "wysoka", "umiarkowana", "niska","bardzo_niska" };
-        String przeznaczenia[] = { "miasto_trasa_teren", "miasto_trasa", "miasto", "teren", "trasa" };
-        String funkcje[] = { "miejska_taniocha", "szybka_bestia", "rodzinne" };
-        String rozmiary[] = { "maly_smerf", "fistaszek" };
+    private static void run() {
+        boolean first = true;
+        boolean logiczny[] = {true, false};
+        String ceny[] = {"bardzo wysoka", "wysoka", "umiarkowana", "niska", "bardzo niska"};
+        String pojemnosciSilnika[] = {"bardzo duza", "duza", "srednia", "mala", "bardzo mala"};
+        String bagazniki[] = {"maly", "obszerny", "praktyczny"};
+        String ilosciMiejsc[] = {"2", "4", "5", "5+"};
+        String wymiary[] = {"bardzo duze", "duze", "srednie", "male", "duze"};
 
         JFrame frame = new JFrame();
         JRadioButton radioButton;
@@ -25,63 +30,231 @@ public class Gui {
         JPanel cenyPanel = new JPanel();
         cenyPanel.setBorder(BorderFactory.createTitledBorder("Cena"));
         final ButtonGroup cenyGroup = new ButtonGroup();
-        for (int i = 0; i < ceny.length; i++) {
-            cenyPanel.add(radioButton = new JRadioButton(ceny[i]));
-            radioButton.setActionCommand(ceny[i]);
+        for (String cena : ceny) {
+            cenyPanel.add(radioButton = new JRadioButton(cena));
+            radioButton.setActionCommand(cena);
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
             cenyGroup.add(radioButton);
         }
 
-        JPanel przeznaczeniaPanel = new JPanel();
-        przeznaczeniaPanel.setBorder(BorderFactory.createTitledBorder("Przeznaczenie"));
-        final ButtonGroup przeznaczeniaGroup = new ButtonGroup();
-        for (int i = 0; i < przeznaczenia.length; i++) {
-            przeznaczeniaPanel.add(radioButton = new JRadioButton(przeznaczenia[i]));
-            radioButton.setActionCommand(przeznaczenia[i]);
-            przeznaczeniaGroup.add(radioButton);
+        first = true;
+        JPanel pojemnosciSilnikaPanel = new JPanel();
+        pojemnosciSilnikaPanel.setBorder(BorderFactory.createTitledBorder("Pojemnosc silnika"));
+        final ButtonGroup pojemnosciSilnikaGroup = new ButtonGroup();
+        for (String pojemnoscSilnika : pojemnosciSilnika) {
+            pojemnosciSilnikaPanel.add(radioButton = new JRadioButton(pojemnoscSilnika));
+            radioButton.setActionCommand(pojemnoscSilnika);
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            pojemnosciSilnikaGroup.add(radioButton);
         }
 
-        JPanel funkcjePanel = new JPanel();
-        funkcjePanel.setBorder(BorderFactory.createTitledBorder("Funkcja"));
-        final ButtonGroup funkcjeGroup = new ButtonGroup();
-        for (int i = 0; i < funkcje.length; i++) {
-            funkcjePanel.add(radioButton = new JRadioButton(funkcje[i]));
-            radioButton.setActionCommand(funkcje[i]);
-            funkcjeGroup.add(radioButton);
+        first = true;
+        JPanel bagaznikiPanel = new JPanel();
+        bagaznikiPanel.setBorder(BorderFactory.createTitledBorder("Bagaznik"));
+        final ButtonGroup bagaznikiGroup = new ButtonGroup();
+        for (String bagaznik : bagazniki) {
+            bagaznikiPanel.add(radioButton = new JRadioButton(bagaznik));
+            radioButton.setActionCommand(bagaznik);
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            bagaznikiGroup.add(radioButton);
         }
 
-        JPanel rozmiaryPanel = new JPanel();
-        rozmiaryPanel.setBorder(BorderFactory.createTitledBorder("Rozmiar"));
-        final ButtonGroup rozmiaryGroup = new ButtonGroup();
-        for (int i = 0; i < rozmiary.length; i++) {
-            rozmiaryPanel.add(radioButton = new JRadioButton(rozmiary[i]));
-            radioButton.setActionCommand(rozmiary[i]);
-            rozmiaryGroup.add(radioButton);
+        first = true;
+        JPanel ilosciMiejscPanel = new JPanel();
+        ilosciMiejscPanel.setBorder(BorderFactory.createTitledBorder("Ilosc miejsc"));
+        final ButtonGroup ilosciMiejscGroup = new ButtonGroup();
+        for (String iloscMiejsc : ilosciMiejsc) {
+            ilosciMiejscPanel.add(radioButton = new JRadioButton(iloscMiejsc));
+            radioButton.setActionCommand(iloscMiejsc);
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            ilosciMiejscGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel wymiaryPanel = new JPanel();
+        wymiaryPanel.setBorder(BorderFactory.createTitledBorder("Wymiary"));
+        final ButtonGroup wymiaryGroup = new ButtonGroup();
+        for (String wymiar : wymiary) {
+            wymiaryPanel.add(radioButton = new JRadioButton(wymiar));
+            radioButton.setActionCommand(wymiar);
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            wymiaryGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel przeznaczenieMiastoPanel = new JPanel();
+        przeznaczenieMiastoPanel.setBorder(BorderFactory.createTitledBorder("Przeznaczenie - miasto"));
+        final ButtonGroup przeznaczenieMiastoGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            przeznaczenieMiastoPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            przeznaczenieMiastoGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel przeznaczenieTerenPanel = new JPanel();
+        przeznaczenieTerenPanel.setBorder(BorderFactory.createTitledBorder("Przeznaczenie - teren"));
+        final ButtonGroup przeznaczenieTerenGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            przeznaczenieTerenPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            przeznaczenieTerenGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel przeznaczenieTrasaPanel = new JPanel();
+        przeznaczenieTrasaPanel.setBorder(BorderFactory.createTitledBorder("Przeznaczenie - trasa"));
+        final ButtonGroup przeznaczenieTrasaGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            przeznaczenieTrasaPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            przeznaczenieTrasaGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel przeznaczenieZazdroscSasiadowPanel = new JPanel();
+        przeznaczenieZazdroscSasiadowPanel.setBorder(BorderFactory.createTitledBorder("Przeznaczenie - zazdrosc sasiadow"));
+        final ButtonGroup przeznaczenieZazdroscSasiadowGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            przeznaczenieZazdroscSasiadowPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            przeznaczenieZazdroscSasiadowGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel bogateWyposazeniePanel = new JPanel();
+        bogateWyposazeniePanel.setBorder(BorderFactory.createTitledBorder("Bogate wyposazenie"));
+        final ButtonGroup bogateWyposazenieGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            bogateWyposazeniePanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            bogateWyposazenieGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel komfortJazdyPanel = new JPanel();
+        komfortJazdyPanel.setBorder(BorderFactory.createTitledBorder("Komfort jazdy"));
+        final ButtonGroup komfortJazdyGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            komfortJazdyPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            komfortJazdyGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel niskieKosztyEksploatacjiPanel = new JPanel();
+        niskieKosztyEksploatacjiPanel.setBorder(BorderFactory.createTitledBorder("Niskie koszty eksploatacji"));
+        final ButtonGroup niskieKosztyEksploatacjiGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            niskieKosztyEksploatacjiPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            niskieKosztyEksploatacjiGroup.add(radioButton);
+        }
+
+        first = true;
+        JPanel wysokieOsiagiPanel = new JPanel();
+        wysokieOsiagiPanel.setBorder(BorderFactory.createTitledBorder("Wysokie osiagi"));
+        final ButtonGroup wysokieOsiagiGroup = new ButtonGroup();
+        for (boolean aLogiczny : logiczny) {
+            wysokieOsiagiPanel.add(radioButton = new JRadioButton(String.valueOf(aLogiczny)));
+            radioButton.setActionCommand(String.valueOf(aLogiczny));
+            if (first) {
+                radioButton.setSelected(true);
+                first = false;
+            }
+            wysokieOsiagiGroup.add(radioButton);
         }
 
         frame.setLayout(new GridLayout(5, 1));
         frame.add(cenyPanel);
-        frame.add(przeznaczeniaPanel);
-        frame.add(funkcjePanel);
-        frame.add(rozmiaryPanel);
+        frame.add(pojemnosciSilnikaPanel);
+        frame.add(bagaznikiPanel);
+        frame.add(ilosciMiejscPanel);
+        frame.add(wymiaryPanel);
+        frame.add(przeznaczenieMiastoPanel);
+        frame.add(przeznaczenieTerenPanel);
+        frame.add(przeznaczenieTrasaPanel);
+        frame.add(przeznaczenieZazdroscSasiadowPanel);
+        frame.add(bogateWyposazeniePanel);
+        frame.add(komfortJazdyPanel);
+        frame.add(niskieKosztyEksploatacjiPanel);
+        frame.add(wysokieOsiagiPanel);
+
         JPanel carAdvisorPanel = new JPanel();
-        JButton chooseButton = new JButton("Wybierz");
+        JButton chooseButton = new JButton("Szukaj");
         carAdvisorPanel.add(chooseButton);
         frame.add(carAdvisorPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(700, 700);
+        frame.setVisible(true);
+
+        UserPreferences preferences = new UserPreferences();
 
         chooseButton.addActionListener(ae -> {
-            String cena = cenyGroup.getSelection().getActionCommand();
-            String przeznaczenie = przeznaczeniaGroup.getSelection().getActionCommand();
-            String funkcja = funkcjeGroup.getSelection().getActionCommand();
-            String rozmiar = rozmiaryGroup.getSelection().getActionCommand();
-            try {
-                System.out.println("Cena " + cena + " przeznaczenie " + przeznaczenie + " funkcja " + funkcja + " rozmiar " + rozmiar);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            preferences.setCena(cenyGroup.getSelection().getActionCommand());
+            preferences.setPojemnoscSilnika(pojemnosciSilnikaGroup.getSelection().getActionCommand());
+            preferences.setBagaznik(bagaznikiGroup.getSelection().getActionCommand());
+            preferences.setIloscMiejsc(ilosciMiejscGroup.getSelection().getActionCommand());
+            preferences.setWymiary(wymiaryGroup.getSelection().getActionCommand());
+            preferences.setPrzeznaczenieMiasto(Boolean.valueOf(przeznaczenieMiastoGroup.getSelection().getActionCommand()));
+            preferences.setPrzeznaczenieTeren(Boolean.valueOf(przeznaczenieTerenGroup.getSelection().getActionCommand()));
+            preferences.setPrzeznaczenieTrasa(Boolean.valueOf(przeznaczenieTrasaGroup.getSelection().getActionCommand()));
+            preferences.setPrzeznaczenieZazdroscSasiadow(Boolean.valueOf(przeznaczenieZazdroscSasiadowGroup.getSelection().getActionCommand()));
+            preferences.setBogateWyposazenie(Boolean.valueOf(bogateWyposazenieGroup.getSelection().getActionCommand()));
+            preferences.setKomfortJazdy(Boolean.valueOf(komfortJazdyGroup.getSelection().getActionCommand()));
+            preferences.setNiskieKosztyEksploatacji(Boolean.valueOf(niskieKosztyEksploatacjiGroup.getSelection().getActionCommand()));
+            preferences.setWysokieOsiagi(Boolean.valueOf(wysokieOsiagiGroup.getSelection().getActionCommand()));
+            String chosenCar = chooseCar(preferences);
+            JOptionPane.showMessageDialog(frame, chosenCar);
         });
+    }
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setVisible(true);
+    private static String chooseCar (UserPreferences preferences) {
+        boolean ifCarFoundFlag = false;
+        if (ifCarFoundFlag) return "Opel Astra";
+        else return "Nie udalo sie";
     }
 }
